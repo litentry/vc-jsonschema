0c8d6e76e081fc25084fb3dd46f1f6f3aadcd3e3
Jonathan Alvarez <jonathan@litentry.com>
2023-12-11 18:42:44 -0500

    schemas: export as schemas
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/1-a1/1-0-0.test.ts
+++ packages/schemas/src/lib/1-a1/1-0-0.test.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
-import { globSync as glob } from 'fast-glob';
 
 import { ajv } from '../ajv';
-import { a1 as schema } from './1-0-0';
+import { schema } from './1-0-0';
 
 describe('0-a1/1-0-0', () => {
   it('should work', () => {
