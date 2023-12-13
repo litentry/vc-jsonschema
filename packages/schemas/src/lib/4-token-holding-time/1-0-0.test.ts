c33956ea6f598e03501c6051e3af9a2a5732bfb1
Jonathan Alvarez <jonathan@litentry.com>
2023-12-13 09:47:05 -0500

    cover a11 with token-holding-time
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/4-token-holding-time/1-0-0.test.ts
+++ packages/schemas/src/lib/4-token-holding-time/1-0-0.test.ts
@@ -4,14 +4,16 @@ import { ajv } from '../ajv';
 import { schema } from './1-0-0';
 
 const tokenHoldingTimeExamples = [
-  'examples/a4/a4.json',
-  'examples/a7/a7.json',
-  'examples/a10/a10.json',
+  'examples/a4/a4.json', // LIT
+  'examples/a7/a7.json', // DOT
+  'examples/a10/a10.json', // WBTC
+  'examples/a11/a11.json', // ETH
 ];
+
+const validate = ajv.compile(schema);
 
 describe('4-token-holding-time/1-0-0', () => {
   it.each(tokenHoldingTimeExamples)('should work for %s', (example) => {
-    const validate = ajv.compile(schema);
     expect(validate.errors).toBeNull();
 
     const json = JSON.parse(fs.readFileSync(example, 'utf8'));
