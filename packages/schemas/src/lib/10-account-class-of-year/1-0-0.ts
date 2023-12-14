6c7a56b1ff444f812d352351a098fc73613f5780
Jonathan Alvarez <jonathan@litentry.com>
2023-12-14 12:34:15 -0500

    add account-class-of-year
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

---
+++ packages/schemas/src/lib/10-account-class-of-year/1-0-0.ts
@@ -0,0 +1,33 @@
+import { JSONSchema7 } from 'json-schema';
+
+import { schema as base } from '../0-base/1-0-0';
+import { resolveGitHubPath } from '../helpers';
+import { credentialSubject, assertion } from '../schema-helpers';
+
+export const schema: JSONSchema7 = {
+  ...base,
+
+  $id: resolveGitHubPath('10-account-class-of-year/1-0-0.json'),
+
+  title: 'Account Class Of Year',
+  description:
+    'The class of year that the user account was created on a particular network (must have on-chain records)',
+
+  properties: {
+    ...base.properties,
+
+    credentialSubject: credentialSubject({
+      title:
+        'Credential Subject of Achainable assertion/ Account Class Of Year',
+      assertions: assertion.and({
+        items: [
+          assertion.clause({
+            src: '$account_created_year',
+            op: '==',
+            dst: undefined, // value is set by the issuer
+          }),
+        ],
+      }),
+    }),
+  },
+};
