09e57445abfccdd4586bbcdb8148b1951945e554
Jonathan Alvarez <jonathan@litentry.com>
2023-12-26 15:58:50 -0500

    add 21-vip3-card-holder`
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/21-vip3-card-holder/1-0-0.test.ts
+++ packages/schemas/src/lib/21-vip3-card-holder/1-0-0.test.ts
@@ -3,14 +3,14 @@ import * as fs from 'fs';
 import { ajv } from '../ajv';
 import { schema } from './1-0-0';
 
-describe('20-weirdoghostgang-holder/1-0-0', () => {
+describe('21-vip3-card-holder/1-0-0', () => {
   it.skip('should work', () => {
     const validate = ajv.compile(schema);
     expect(validate.errors).toBeNull();
 
     const json = JSON.parse(
       fs.readFileSync(
-        'examples/20-weirdoghostgang-holder/weirdoghostgang-holder.json',
+        'examples/21-vip3-card-holder/vip3-card-holder.json',
         'utf8'
       )
     );
