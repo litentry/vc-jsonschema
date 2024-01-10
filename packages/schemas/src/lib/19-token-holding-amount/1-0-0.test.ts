e7d987ec2839b4fb7bbaafee25a722a35903b9e0
Jonathan Alvarez <jonathan@litentry.com>
2024-01-10 17:37:25 -0500

    chore: add examples for 10 token holding amount (#11)
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/19-token-holding-amount/1-0-0.test.ts
+++ packages/schemas/src/lib/19-token-holding-amount/1-0-0.test.ts
@@ -1,19 +1,18 @@
 import * as fs from 'fs';
+import { globSync as glob } from 'fast-glob';
 
 import { ajv } from '../ajv';
 import { schema } from './1-0-0';
 
+const jsonFiles = glob('examples/19-token-holding-amount/*.json');
+
 describe('19-token-holding-amount/1-0-0', () => {
-  it.skip('should work', () => {
+  it.each(jsonFiles)('should work for %s', (example) => {
     const validate = ajv.compile(schema);
+
     expect(validate.errors).toBeNull();
 
-    const json = JSON.parse(
-      fs.readFileSync(
-        'examples/token-holding-amount/19-token-holding-amount.json',
-        'utf8'
-      )
-    );
+    const json = JSON.parse(fs.readFileSync(example, 'utf8'));
     const valid = validate(json);
 
     if (!valid) console.log(validate.errors);
