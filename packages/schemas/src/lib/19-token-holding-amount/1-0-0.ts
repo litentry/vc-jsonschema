ba606b8404ea58e4a692bba40479343c95325df4
Jonathan Alvarez <jonathan@litentry.com>
2024-02-06 17:00:19 -0500

    undo duplicate id entry
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/19-token-holding-amount/1-0-0.ts
+++ packages/schemas/src/lib/19-token-holding-amount/1-0-0.ts
@@ -27,7 +27,6 @@ const supportedTokens = [
   '$eth_holding_amount',
   // https://github.com/litentry/litentry-parachain/blob/dev/tee-worker/litentry/core/credentials/src/achainable/lit_holding_amount.rs
   'lit_holding_amount',
-  '$lit_holding_amount',
   /// https://github.com/litentry/litentry-parachain/blob/dev/tee-worker/litentry/core/credentials/src/litentry_profile/lit_staking.rs
   '$lit_staking_amount',
 ];
