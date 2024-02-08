7c53d89086f67a5d7918dc0dbc65c10b568f57c9
Jonathan Alvarez <jonathan@litentry.com>
2024-02-08 11:27:39 -0500

    feat: add karatdao schema
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

---
+++ packages/schemas/src/lib/25-platform-user/1-0-0.ts
@@ -0,0 +1,36 @@
+import { JSONSchema7 } from 'json-schema';
+
+import { schema as base } from '../0-base/1-0-0';
+import { resolveGitHubPath } from '../helpers';
+import { credentialSubject, assertion } from '../schema-helpers';
+
+const supportedPlatforms = [
+  // https://github.com/litentry/litentry-parachain/blob/dev/tee-worker/litentry/core/credentials-v2/src/platform_user/mod.rs
+  'KaratDao',
+];
+
+export const schema: JSONSchema7 = {
+  ...base,
+
+  $id: resolveGitHubPath('25-platform-user/1-0-0.json'),
+
+  title: 'Platform user',
+  description: 'You are a user of a certain platform',
+
+  properties: {
+    ...base.properties,
+
+    credentialSubject: credentialSubject({
+      title: 'Credential Subject of Platform user',
+      assertions: assertion.and({
+        items: [
+          assertion.clause({
+            src: ['$platform'],
+            op: ['=='],
+            dst: supportedPlatforms,
+          }),
+        ],
+      }),
+    }),
+  },
+};
