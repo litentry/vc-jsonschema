4de14ddb0a6316f8bd0a3e515f41685f41d2ca0c
Jonathan Alvarez <jonathan@litentry.com>
2024-02-08 16:43:24 -0500

    chore: use same schema for achainable basic and achainable amount (#17)
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

---
+++ packages/schemas/src/lib/17-token-holding-amount/1-0-0.test.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+import { globSync as glob } from 'fast-glob';
+
+import { ajv } from '../ajv';
+import { schema } from './1-0-0';
+
+const jsonFiles = glob('examples/17-token-holding-amount/*.json');
+
+describe('17-token-holding-amount/1-0-0', () => {
+  it.each(jsonFiles)('should work for %s', (example) => {
+    const validate = ajv.compile(schema);
+
+    expect(validate.errors).toBeNull();
+
+    const json = JSON.parse(fs.readFileSync(example, 'utf8'));
+    const valid = validate(json);
+
+    if (!valid) console.log(validate.errors);
+
+    expect(valid).toBeTruthy();
+  });
+});
