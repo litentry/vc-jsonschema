55130abf58214514f4e5cdf8f964b2d6c1554a02
Jonathan Alvarez <jonathan@litentry.com>
2024-02-14 13:16:41 -0500

    moves ajv intance setup
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/ajv.ts
+++ packages/schemas/src/lib/ajv.ts
@@ -1,16 +1,1 @@
-import Ajv from 'ajv';
-import addFormats from 'ajv-formats';
-
-// See options https://ajv.js.org/options.html
-const ajv = new Ajv({
-  // CryptoSummary's assertion clauses have dynamic size, so we need to disable strict tuples.
-  strictTuples: false,
-});
-
-addFormats(ajv, {
-  mode: 'fast',
-  formats: ['date-time', 'uri'],
-  keywords: true,
-});
-
-export { ajv };
+export { ajv } from '@litentry/vc-schema-validator';
