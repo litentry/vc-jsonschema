f60d1954c1d993a5678dac73a47289259314fcf0
Jonathan Alvarez <jonathan@litentry.com>
2024-02-29 11:00:26 -0500

    fixes for weirdoGhostGang variation
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- packages/schemas/src/lib/25-nft-holder/1-0-0.ts
+++ packages/schemas/src/lib/25-nft-holder/1-0-0.ts
@@ -35,7 +35,7 @@ export const schema: JSONSchema7 = {
                 assertion.clause({
                   src: ['$network'],
                   op: ['=='],
-                  dst: ['bsc', 'polygon', 'arbitrum'],
+                  dst: ['bsc', 'polygon', 'arbitrum', 'ethereum'],
                 }),
                 assertion.clause({
                   src: ['$address'],
@@ -44,6 +44,7 @@ export const schema: JSONSchema7 = {
                     '0x9f488C0dafb1B3bFeeD3e886e0E6E5f3f4517925',
                     '0xAc2e4e67cffa5E82bfA1e169e5F9aa405114C982',
                     '0xcccFF19FB8a4a2A206d07842b8F8c8c0A11904C2',
+                    '0x9401518f4EBBA857BAA879D9f76E1Cc8b31ed197',
                   ],
                 }),
               ],
